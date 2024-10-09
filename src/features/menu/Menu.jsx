581adab58e5ccbf2f80199b8a1aa2db4a9d9fbc7
Gman271 <gkovcs71@gmail.com>
2024-10-09 17:48:02 +0200

    Loader comp added, Error compconfigured

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,7 +4,6 @@ import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
 
   return (
     <ul>
@@ -15,6 +14,7 @@ function Menu() {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export async function loader() {
   const menu = await getMenu();
   return menu;
